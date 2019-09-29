f9cdcb5b53b4bbf82d210ef689e047aa7763d958
Dario Cangialosi <dario.cangialosi@gmail.com>
2019-09-29 20:43:40 +0200

    session var

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
 public class HelloWorldServlet extends HttpServlet 
@@ -16,8 +17,24 @@ public class HelloWorldServlet extends HttpServlet
   HttpServletResponse response) 
       throws IOException
   {
+
+      response.getOutputStream().println("<!doctype html>");
+
+// import javax.servlet.http.HttpSession;
     // http://localhost:8080/?paramName=paramValue
-      response.getOutputStream().print("Hello World "+
-      request.getParameter("paramName"));
+      response.getOutputStream().println("Hello World "+
+      request.getParameter("paramName")+"<br>");
+
+response.getOutputStream().println("(reload to test session)<br>");
+
+  HttpSession session=request.getSession();
+        session.setAttribute("counter",
+session.getAttribute("counter")==null?
+"0":new Integer(1+Integer.valueOf(session.getAttribute("counter").toString())).toString()
+);
+
+      response.getOutputStream().print(session.getAttribute("counter").toString());
+
+
   }
 }
