4f2079efd13f6a70141acc3397211655c1adb363
Dario Cangialosi <dario.cangialosi@gmail.com>
2019-09-29 15:47:17 +0200

    with param

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -9,10 +9,15 @@ import java.io.IOException;
 @WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
 public class HelloWorldServlet extends HttpServlet 
 {
+  private static final long serialVersionUID = 1L;
+
   @Override 
-  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+  public void service(HttpServletRequest request,
+  HttpServletResponse response) 
       throws IOException
   {
-      response.getOutputStream().print("Hello World");
+    // http://localhost:8080/?paramName=paramValue
+      response.getOutputStream().print("Hello World "+
+      request.getParameter("paramName"));
   }
 }
